557f19271cb40a7505f52548eb7d005bb82f0424
Hemanga-Hamal <a1923168@adelaide.edu>
2024-09-02 00:36:47 +0930

    NETWORK< NEED LOOK AT

--- Stats_Net_down.cpp
+++ Stats_Net_down.cpp
@@ -1,7 +1,43 @@
+//SEE WINDOWS PERFORMANCE COUNTER
+
 #include <windows.h>
+#include <pdh.h>
 #include <iostream>
+#include <string>
+
+#pragma comment(lib, "pdh.lib")
 
 int main() {
-    
-    return 0; 
+    PDH_HQUERY query;
+    PDH_HCOUNTER counter;
+    PDH_FMT_COUNTERVALUE counterVal;
+
+    std::string networkInterface = "Ethernet";  // Replace with your network interface name
+    std::wstring counterPath = L"\\Network Interface(" + std::wstring(networkInterface.begin(), networkInterface.end()) + L")\\Bytes Total/sec";
+
+    // Open a query object.
+    PdhOpenQuery(NULL, NULL, &query);
+
+    // Add the counter to the query.
+    PdhAddCounterW(query, counterPath.c_str(), NULL, &counter);
+
+    // Collect data for the first time.
+    PdhCollectQueryData(query);
+
+    // Sleep for 1 second to get the network speed.
+    Sleep(1000);
+
+    // Collect data again.
+    PdhCollectQueryData(query);
+
+    // Get the formatted counter value.
+    PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, NULL, &counterVal);
+
+    // Close the query.
+    PdhCloseQuery(query);
+
+    // Display network speed in KBps
+    std::cout << "Network speed: " << counterVal.doubleValue / 1024.0 << " KBps" << std::endl;
+
+    return 0;
 }
