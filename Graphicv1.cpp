47f7151c067cf706af47c33d041348738078497c
Hemanga-Hamal <hemanga.hamal@student.adelaide.edu.au>
2024-10-19 19:40:31 +1030

    ALl text is done, as well as test files GraphicV1

--- Graphicv1.cpp
+++ Graphicv1.cpp
@@ -1,5 +1,6 @@
 // custom libraries
 #include "raylib.h"
+#include "BarV1.h"
 #include "GaugeV1.h"
 // standard libraries
 #include <string>
@@ -17,7 +18,7 @@ int main() {
     int screenWidth = baseWidth;
     int screenHeight = baseHeight;
 
-    InitWindow(screenWidth, screenHeight, "Performance Monitor - GaugeV1");
+    InitWindow(screenWidth, screenHeight, "Performance Monitor - GraphicV1");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetWindowMinSize(300, 300);
     SetTargetFPS(60);
@@ -28,16 +29,38 @@ int main() {
     GaugeV1 gaugeArc(GaugeV1::Theme(), GaugeV1::Dimensions(), GaugeV1::Config::ConfigArc());
     GaugeV1 gaugeQuarter(GaugeV1::Theme(), GaugeV1::Dimensions(), GaugeV1::Config::ConfigQuarter());
 
-    GaugeV1::Config Config;
-    Config.startAngle = 10.0f;
-    Config.totalAngle = 10.0f;
-    Config.autoScale = true;
-    Config.screenSizeRatio = 0.3f;
-    Config.method = 1;
+    // Bar instances
+    BarV1::Theme barTheme;
+    barTheme.barBackgroundColor = DARKGRAY;
+    barTheme.barForegroundColor = GREEN;
+    barTheme.textColor = WHITE;
 
-    GaugeV1 gaugeTest(GaugeV1::Theme(), GaugeV1::Dimensions(), Config);
+    BarV1::Dimensions barDims;
+    barDims.barWidth = 200;
+    barDims.barHeight = 30;
+    barDims.scalingRatio = 1.0f;
+    barDims.textSizeRatio = 0.7f;
+    barDims.minSize = 90;
+    barDims.maxSize = 350;
 
-    // Gauge value
+    BarV1::Config barConfig(0.0f, 100.0f);
+    barConfig.autoScale = true;
+    barConfig.screenSizeRatio = 0.25f;
+
+    BarV1 bar1(barTheme, barDims, barConfig);
+    BarV1 bar2(barTheme, barDims, barConfig);
+
+    // Custom gauge config
+    GaugeV1::Config gaugeConfig;
+    gaugeConfig.startAngle = 10.0f;
+    gaugeConfig.totalAngle = 10.0f;
+    gaugeConfig.autoScale = true;
+    gaugeConfig.screenSizeRatio = 0.3f;
+    gaugeConfig.method = 1;
+
+    GaugeV1 gaugeTest(GaugeV1::Theme(), GaugeV1::Dimensions(), gaugeConfig);
+
+    // Test value
     float testValue = 0.0f;
 
     while (!WindowShouldClose()) {
@@ -50,10 +73,19 @@ int main() {
         Vector2 quad3 = { screenWidth * 0.25f, screenHeight * 0.75f };
         Vector2 quad4 = { screenWidth * 0.75f, screenHeight * 0.75f };
 
-        // Update gauge value
+        // Update values
         updateMetrics(testValue, frameCounter);
         gaugeArc.setValue(testValue);
         gaugeQuarter.setValue(testValue);
+        
+        // Update bar configurations with new test value
+        BarV1::Config newConfig1 = bar1.getConfig();
+        newConfig1.value = testValue;
+        bar1.setConfig(newConfig1);
+
+        BarV1::Config newConfig2 = bar2.getConfig();
+        newConfig2.value = std::min(100.0f - testValue, 100.0f); // Inverse of first bar
+        bar2.setConfig(newConfig2);
 
         // Start drawing
         BeginDrawing();
@@ -64,12 +96,15 @@ int main() {
             DrawLine(0, screenHeight / 2, screenWidth, screenHeight / 2, WHITE);
             DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
 
-            // Draw gauges in different quadrants
-            gaugeArc.draw(quad1, "test");
-            gaugeQuarter.draw(quad2,"test" ); 
-            gaugeTest.draw(quad3, "test" );
+            // Draw components in different quadrants
+            gaugeArc.draw(quad1, "Gauge Arc");
+            gaugeQuarter.draw(quad2, "Gauge Quarter");
+            
+            // Draw bars
+            bar1.draw(quad3, "Bar Test 1", std::to_string(static_cast<int>(testValue)));
+            bar2.draw(quad4, "Bar Test 2", std::to_string(static_cast<int>(100.0f - testValue)));
 
-            // Center debug points for quadrants
+            // Debug points for quadrant centers
             DrawCircle(static_cast<int>(quad1.x), static_cast<int>(quad1.y), 2, RED);
             DrawCircle(static_cast<int>(quad2.x), static_cast<int>(quad2.y), 2, RED);
             DrawCircle(static_cast<int>(quad3.x), static_cast<int>(quad3.y), 2, RED);
